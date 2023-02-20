e3ba049fcd07f4a6dfe5a0ad213ea6f4b14b576b
Alessandro Ontani <casualotn@gmail.com>
2023-02-20 15:17:31 +0100

    removed useless import

--- src/components/MainSearch.jsx
+++ src/components/MainSearch.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Container, Row, Col, Form } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { jobFetcher } from "../redux/actions";
